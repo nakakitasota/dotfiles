a0fe9d574dab1f9f9a488f63963761c2e9f69c60
Sota Nakakita <54422939+nakakitasota@users.noreply.github.com>
2022-06-29 16:46:54 +0900

    [firefox] Set font fallback order for Japanese

--- etc/firefox/user.js
+++ etc/firefox/user.js
@@ -1,4 +1,5 @@
 user_pref("general.useragent.locale", "ja"); // Webページ表示言語
+user_pref("font.cjk_pref_fallback_order", "ja,zh-cn,zh-hk,zh-tw,ko"); // Font fallback order for Japanese
 user_pref("general.autoScroll", true); // Enable auto-scrolling
 user_pref("browser.startup.homepage", "www.google.co.jp"); // ホームページをGoogleにセット
 user_pref("browser.shell.checkDefaultBrowser", false); // 起動時に既定のブラウザを確認しない
