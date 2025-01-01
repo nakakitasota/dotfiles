45dcc0a616d1726df4293463652da852a18d92c4
Sota Nakakita <54422939+nakakitasota@users.noreply.github.com>
2025-01-01 18:21:14 +0900

    [nvim] set keymap for LSP operations

--- .vim/rc/nvim-lsp.rc.lua
+++ .vim/rc/nvim-lsp.rc.lua
@@ -1,3 +1,24 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Attach key mappings for LSP functionalities",
+    callback = function ()
+        vim.keymap.set('n', '[LSP]', '<Nop>')
+        vim.keymap.set('n', '<Space>l', '[LSP]', {remap = true})
+
+        vim.keymap.set('n', '[LSP]r', '<cmd>lua vim.lsp.buf.rename()<CR>')
+        vim.keymap.set('n', '[LSP]a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+        vim.keymap.set('n', '[LSP]f',     '<cmd>lua vim.lsp.buf.formatting()<CR>')
+        vim.keymap.set('n', 'K',      '<cmd>lua vim.lsp.buf.hover()<CR>')
+        vim.keymap.set('n', 'gr',     '<cmd>lua vim.lsp.buf.references()<CR>')
+        vim.keymap.set('n', 'gd',     '<cmd>lua vim.lsp.buf.definition()<CR>')
+        vim.keymap.set('n', 'gD',     '<cmd>lua vim.lsp.buf.declaration()<CR>')
+        vim.keymap.set('n', 'gi',     '<cmd>lua vim.lsp.buf.implementation()<CR>')
+        vim.keymap.set('n', 'gt',     '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+        vim.keymap.set('n', 'ge',     '<cmd>lua vim.diagnostic.open_float()<CR>')
+        vim.keymap.set('n', 'g]',     '<cmd>lua vim.diagnostic.goto_next()<CR>')
+        vim.keymap.set('n', 'g[',     '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+    end
+})
+
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
     vim.lsp.handlers.hover, {
         border = "single"
