87eaa856c42c1913c152db1a530bcf54f11d28cd
Sota Nakakita <54422939+nakakitasota@users.noreply.github.com>
2025-05-01 18:49:43 +0900

    [nvim] use nvim 0.11 'winborder' option

--- .vim/rc/nvim-lsp.rc.lua
+++ .vim/rc/nvim-lsp.rc.lua
@@ -19,16 +19,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 })
 
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover, {
-        border = "single"
-    }
-)
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signatureHelp, {
-        border = "single"
-    }
-)
 vim.diagnostic.config({ float = { border = "single" } })
 
 vim.diagnostic.config({
