13a5a2fc414f50d297b4ea87549f6cd1c9b9d668
Sota Nakakita <54422939+nakakitasota@users.noreply.github.com>
2026-02-22 14:55:04 +0900

    [*] Use Ghostty Terminal Emulator on macOS

---
+++ .bin/new-terminal.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/osascript
+
+tell application "Ghostty"
+    if it is running
+        tell application "System Events" to tell process "Ghostty"
+            click menu item "New Window" of menu "File" of menu bar 1
+        end tell
+        activate
+    else
+        activate
+    end if
+end tell
